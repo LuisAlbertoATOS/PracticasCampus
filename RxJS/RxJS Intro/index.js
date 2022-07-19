812ec749dbf46b11d5814fb95ef8cf7c58579166
Perez <luis-alberto.perez@atos.net>
2022-07-19 10:03:14 -0500

    rxjs intro part 2

--- RxJS/RxJS Intro/index.js
+++ RxJS/RxJS Intro/index.js
@@ -1,5 +1,5 @@
 const { Observable } = require("rxjs");
-const { map } = require("rxjs/operators");
+const { map, pluck } = require("rxjs/operators");
 
 const users = {
     data: [
@@ -26,26 +26,53 @@ const users = {
     ]
 }
 
+const users2 = {
+    data: [
+        {
+            status: "active",
+            age: 22
+        },
+        {
+            status: "active",
+            age: 20
+        },
+        {
+            status: "inactive",
+            age: 18
+        },
+        {
+            status: "active",
+            age: 15
+        },
+        {
+            status: "inactive",
+            age: 13
+        }
+    ]
+}
+
+
 const observable = new Observable((subscriber) => {
     subscriber.next(users);
+    // subscriber.next(users2);
+    // subscriber.complete();
 }).pipe(
+    pluck("data"),
+    filter((value) => value.length >= 5),
     map((value) => {
-        console.log("1...", value);
-        return value.data
-    }),
-    map((value) => {
-        console.log("2...", value);
+        // console.log("2...", value);
         return value.filter(user => user.status === "active");
     }),
     map((value) => {
-        console.log("3...", value);
+        // console.log("3...", value);
         return value.reduce((sum, user) => sum + user.age, 0) / value.length;
     }),
     map((value) => {
-        console.log("4...", value);
+        // console.log("4...", value);
         if (value < 18) throw new Error("Average age is too young");
         else return value;
-    })
+    }),
+    map((average) => `The average age is ${average}`)
 );
 
 const observer = {
@@ -53,6 +80,7 @@ const observer = {
         console.log("Observer got the value of", value) 
     },
     error: (err) => {
+        // when we get here, no other pipes will be executed
         console.log("There was an error:", err);
     },
     complete: () => {
