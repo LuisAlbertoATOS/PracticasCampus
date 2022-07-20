0bdd0be7c8b6bf191603a192c3de39064fcc534b
Perez <luis-alberto.perez@atos.net>
2022-07-20 15:22:08 -0500

    webpack bundler practice

---
+++ JS Bundlers/Webpack Bundler/src/vendor.js
@@ -0,0 +1,3 @@
+import "bootstrap";
+
+alert("Hi from vendor");
---
+++ JS Bundlers/Webpack Bundler/webpack.common.js
@@ -0,0 +1,32 @@
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+module.exports = {
+    entry: {
+        main: "./src/index.js",
+        vendor: "./src/vendor.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    'html-loader'
+                ],
+            },
+            {
+                test: /\.(svg|jpg|png|gif)$/,
+                use: {
+                    options: {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[hash].[ext]",
+                            outputPath: "imgs"
+                        }
+                    }
+                }
+            }
+        ]
+    }
+
+}
